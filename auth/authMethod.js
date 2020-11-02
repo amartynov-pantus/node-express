bdf99adb9f4d6e413c82f4cd2e61489decd989f8
amartynov-pantus <WAcuSUQSe56HsjmZ>
2020-11-02 08:49:07 +0400

    change user data

--- auth/authMethod.js
+++ auth/authMethod.js
@@ -1,16 +1,14 @@
 const createToken = require("./createToken");
 
 function auth(log, pass){
-    if (log == 'admin' && pass == 'admin')
-    {
-      //Создание токена для ИД  пользователя
-      var token=createToken(1); // !!! сделать тот ИД который получу от БД
-   return token;
-      console.log("TOKEN "+token);  
-    }
-    else {
-      //  response.sendStatus(403);
-      return undefined;
+    if (log === 'admin' && pass === '6667') {
+        //Создание токена для ИД  пользователя
+        let token = createToken(1); // !!! сделать тот ИД который получу от БД
+        return token;
+
+    } else {
+        //  response.sendStatus(403);
+        return undefined;
     }
 }
 module.exports = auth;
